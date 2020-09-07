41ca2671f2f91ba67ce4574ab7e46f785c7eeb74
vhxubo <vhxubo@gmail.com>
2020-09-07 10:06:48 +0800

    Feat Q401

---
+++ Q401/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int sum = 0;
+            do
+            {
+                sum += num;
+                num++;
+            } while (num <= 100);
+            Console.Write(sum);
+            Console.ReadKey();
+        }
+    }
+}
