c4b25e03c7a4e9173977d029c5645a7bf38174ae
vhxubo <vhxubo@gmail.com>
2020-09-07 08:58:48 +0800

    Feat Q383

---
+++ Q383/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0;i < 100; i++)
+            {
+                Console.WriteLine("我下次一定要吃大盘鸡");
+            }
+            Console.ReadKey();
+        }
+    }
+}
