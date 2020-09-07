ea52bfcb0c8ec889acaef821a6d987add97a1b3c
vhxubo <vhxubo@gmail.com>
2020-09-07 09:51:43 +0800

    Feat Q396

---
+++ Q396/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q396
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (true)
+            {
+                x += 1;
+                if (x == 3)
+                {
+                    break;
+                }
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
