eaff35902cbde1aaf11ebc349caaeea85f4429da
vhxubo <vhxubo@gmail.com>
2020-09-07 09:06:16 +0800

    Feat Q386

---
+++ Q386/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q386
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            if (x < 10)
+            {
+                Console.WriteLine("小于");
+            }
+            Console.ReadKey();
+        }
+    }
+}
