e9767b42d9a553fcfe0dd0ef5903494a752471c9
vhxubo <vhxubo@gmail.com>
2020-09-07 10:51:25 +0800

    Feat Q406

---
+++ Q406/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q406
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = (a + 4) * 3;
+            Console.Write(b);
+            Console.ReadKey();
+        }
+    }
+}
