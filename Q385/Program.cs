490d41f8c6c947275388075e50dfc1c6b70c956a
vhxubo <vhxubo@gmail.com>
2020-09-07 09:03:44 +0800

    Feat Q385

---
+++ Q385/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q385
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            if (age >= 18)
+            {
+                Console.WriteLine("成年");
+            }
+            else if(age < 18)
+            {
+                Console.WriteLine("未成年");
+            }
+            Console.ReadKey();
+        }
+    }
+}
