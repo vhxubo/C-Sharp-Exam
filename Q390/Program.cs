2d130e1e245eaebd1c04dcbe45752121aef637b4
vhxubo <vhxubo@gmail.com>
2020-09-07 09:40:02 +0800

    Feat Q390

---
+++ Q390/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q390
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            do
+            {
+                name = Console.ReadLine();
+            }
+            while (name != "q");
+        }
+    }
+}
