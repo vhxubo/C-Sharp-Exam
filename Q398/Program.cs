510febf90d31760aa7bf96eb1f9bf131ea92f513
vhxubo <vhxubo@gmail.com>
2020-09-07 09:56:22 +0800

    Feat Q398

---
+++ Q398/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q398
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int i = 0;
+
+            while (name != "q")
+            {
+                name = Console.ReadLine();
+                i++;
+                if (i == 5)
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
