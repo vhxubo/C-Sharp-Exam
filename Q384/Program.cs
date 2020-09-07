e07a0df2857da0215f494bc2451b2c5289d2ebfb
vhxubo <vhxubo@gmail.com>
2020-09-07 09:01:01 +0800

    Feat Q384

---
+++ Q384/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q384
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 12;
+            Console.WriteLine(number1 + number2);
+            Console.ReadKey();
+        }
+    }
+}
