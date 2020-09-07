949c32dc176e62a055bb4de24bb5e780f1e00b4a
vhxubo <vhxubo@gmail.com>
2020-09-07 11:24:15 +0800

    Feat Q408

---
+++ Q408/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q408
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int sum = 0;
+            while (num <= 100)
+            {
+                if (num % 7 == 0)
+                {
+                    num++;
+                    continue;
+                }
+                sum += num;
+                num++;
+            }
+            Console.Write(sum);
+            Console.ReadKey();
+        }
+    }
+}
