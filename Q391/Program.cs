89d85418b6644b7bebe0e8dfaa1293d5ea4488b6
vhxubo <vhxubo@gmail.com>
2020-09-07 09:45:40 +0800

    Feat Q391

---
+++ Q391/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q391
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
