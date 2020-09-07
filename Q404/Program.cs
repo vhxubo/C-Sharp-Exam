b013e88a9e793dcaa884e37f6fd8c6128e339f72
vhxubo <vhxubo@gmail.com>
2020-09-07 10:45:26 +0800

    Feat Q404

---
+++ Q404/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q404
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入您的年龄：");
+            int age = int.Parse(Console.ReadLine());
+            if (age > 23)
+            {
+                Console.Write("您到了结婚的年龄了~");
+            }
+            Console.ReadKey();
+        }
+    }
+}
