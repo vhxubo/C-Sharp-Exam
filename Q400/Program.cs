ec87cd3db918c79a3fc84ff195dfe786c6ea7fa9
vhxubo <vhxubo@gmail.com>
2020-09-07 10:06:34 +0800

    Feat Q400

---
+++ Q400/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q400
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} * {1} = {2}\t", j, i, i * j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
