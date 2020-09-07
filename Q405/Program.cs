2fa576a56dccc91f746358a4ed4ac3a5036e5a06
vhxubo <vhxubo@gmail.com>
2020-09-07 10:51:14 +0800

    Feat Q405

---
+++ Q405/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int num = 1; num <= 5; num++)
+            {
+                sum += num;
+            }
+            Console.Write(sum);
+            Console.ReadKey();
+        }
+    }
+}
