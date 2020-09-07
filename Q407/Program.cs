a1df3c47abfda85ba51bf552917d1a2104e33983
vhxubo <vhxubo@gmail.com>
2020-09-07 10:54:20 +0800

    Feat Q407

---
+++ Q407/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q407
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("我要成为一名出色的程序员");
+            }
+            Console.ReadKey();
+        }
+    }
+}
