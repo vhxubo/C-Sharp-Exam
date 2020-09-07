b6ca162683434de6b52a79e38cb3f9457fd2ca71
vhxubo <vhxubo@gmail.com>
2020-09-07 09:33:42 +0800

    Feat Q389

---
+++ Q389/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q389
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(1, 2));
+            Console.ReadKey();
+        }
+        static int Sum(params int[] args)
+        {
+            int result = 0;
+            foreach (int num in args)
+            {
+                result += num;
+            }
+            return result;
+        }
+    }
+}
